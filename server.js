39d24acbfd496ccb94d8a65d40181c22a8d1869b
708y <toby.myers@gmail.com>
2016-09-12 07:24:49 +0000

    added header JSON output

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express();
+
+app.get('/', function (req, res) {
+    var json;
+    json = JSON.stringify({ipaddress: req.headers['x-forwarded-for'], language: req.headers['accept-language'].split(',')[0], software: req.headers['user-agent'].split('(')[1].split(')')[0]});
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(json);
+});
+
+app.listen(process.env.PORT || 8080)
